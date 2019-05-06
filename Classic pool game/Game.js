555bbbb4ddb512c693df76b57cc6b0025bc90ef1
ManuelRafaelano <manuelrafaelano@gmail.com>
2019-05-06 14:40:11 -0600

    middle

---
+++ Classic pool game/Game.js
@@ -0,0 +1,16 @@
+function Game() {}
+Game.prototype.init = function() {
+  this.gameWorld = new GameWorld();
+};
+Game.prototype.start = function() {
+  PoolGame.init();
+  PoolGame.mainLoop();
+};
+Game.prototype.mainLoop = function() {
+  Canvas.clear();
+  PoolGame.gameWorld.update();
+  PoolGame.gameWorld.draw();
+  Mouse.reset();
+  requestAnimationFrame(PoolGame.mainLoop);
+};
+let PoolGame = new Game();
